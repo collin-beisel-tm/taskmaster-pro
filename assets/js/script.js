b666457d84071aafd610f241fc0305db3b21effd
cbeisel <collin.beisel@toyota.com>
2021-11-10 10:04:44 -0600

    added classes and js for drop zones and trash drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,15 +182,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(this).addClass("dropover-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(this).removeClass("dropover-active");
   },
   update: function(event) {
     // loop over current set of children in sortable list
@@ -236,12 +242,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
   
 });
@@ -274,4 +283,8 @@ var auditTask = function(taskEl) {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
